7e9e316b0c9be2261f9745b46e54692ba8ef81b0
Michael Stadtmiller <MichaelStadtmiller@gmail.com>
2018-04-10 12:51:48 -0400

    13 finished

--- 13-program.js
+++ 13-program.js
@@ -4,15 +4,44 @@ var url = require('url')
 var port = process.argv[2]
 
 var server=http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    var json = url.parse(request.url, true)
-    if (json['pathname'] === '/api/parsetime' ){
-        var time = new Date().toISOString(json['search']) 
-        //need to break out year. month, day into json and return
+    var json = url.parse(request.url, true),
+    path = json.pathname,
+    startTime = json.query.iso,
+    result;
+    
+    if (path === '/api/parsetime' ){
+        result = getTimeObj(startTime);
     }
-    //else if the other path
-        //return whatever that thing needs
-    // server sticks around. need it to stop after receiving the requests.
+    else if (path === '/api/unixtime') {
+        result = getUnixTimeStamp(startTime)
+    }
+
+    if (result){
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(result));
+    } else {
+        response.writeHead(404);
+        response.end();
+    }
 })
 
 server.listen(port)
+
+function getUnixTimeStamp(startTime){
+    return {
+        unixtime: getTimeStamp(startTime)
+    };
+}
+
+function getTimeStamp(startTime) {
+    return Date.parse(startTime);
+}
+
+function getTimeObj(startTime){
+    var date = new Date(getTimeStamp(startTime));
+    return {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+    };
+}
